ef20c328286437f11cfd0965be7d699b7233b813
Noe <31541490+HydroPage90@users.noreply.github.com>
2020-04-23 15:03:25 -0500

    Update 957.py

--- 957.py
+++ 957.py
@@ -1,16 +1,16 @@
-words = {}
+count_dictionary = {}
 
 
-def update_counts(words, word):
-    if word in words:
-        words[word] += 1
+def update_counts(count_dictionary, word):
+    if word in count_dictionary:
+        count_dictionary[word] += 1
     else:
-        words[word] = 1
+        count_dictionary[word] = 1
 
 
 words_in = input("Please enter input:\n\n")
 
 for word in words_in.split():
-    update_counts(words, word)
+    update_counts(count_dictionary, word)
         
-print words
+print count_dictionary
